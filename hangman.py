58d2402e20672694fc49b0422416760f579cb0f1
22MGural <22MGural.acc@lwetb.ie>
2025-02-06 12:00:11 +0000

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,33 +1,33 @@
-
-word = 'money'
-blanks=["_"]*len(word)
-lives=7
-guessed_letters = set()
-print(" ".join(blanks))
-while lives > 0 and "_" in blanks:
-    guess=input("Guess a letter:")
-     if guess in guessed_letters:
-           print("You've already guessed '{guess}'. Guessed letters: {', '.join(sorted(guessed_letters))}")
+while True:
+   word = 'money'
+   blanks=["_"]*len(word)
+   lives=7
+   guessed_letters = set()
+   print(" ".join(blanks))
+   while lives > 0 and "_" in blanks:
+       guess = input("Guess a letter: ")
+       if guess in guessed_letters:
+           print(f"You've already guessed '{guess}'. Guessed letters: {', '.join(sorted(guessed_letters))}")
            continue
        guessed_letters.add(guess)
-    if guess in word:
-        for q, letter in enumerate(word):
-            if letter == guess:
-                blanks[q] = guess
-    else:
-        lives -= 1
-        print("wrong, try again.")
-    print(" ".join(blanks))
-if "_" not in blanks:
-    print("You win")
-else:
-    print("Game Over")
-while True:
+       if guess in word:
+           for q, letter in enumerate(word):
+               if letter == guess:
+                   blanks[q] = guess
+       else:
+           lives -= 1
+           print("wrong, try again.")
+       print(" ".join(blanks))
+   if "_" not in blanks:
+       print("You win")
+   else:
+       print("Game Over")
    replay = input("Do you want to play again? : ")
-   if replay != "yes":
+   if replay == "yes":
+       continue
+   else:
+       print("Goodbye")
        break
-    
-    
     
 
             
