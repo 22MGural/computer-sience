627d20b8e40756a4bc2952e8f39e24afbf40894f
22MGural <22MGural.acc@lwetb.ie>
2025-02-13 12:00:04 +0000

    Add files via upload

---
+++ list Q.py
@@ -0,0 +1,21 @@
+#1
+lst=input('Enter a list:')
+lst=list(lst)
+lst.reverse()
+print(lst)
+
+#2
+lst1 = input("Enter first list: ")
+lst1=list(lst1)
+lst2 = input("Enter second list: ")
+lst2=list(lst2)
+lst3 = lst1 + lst2
+print("Joined List:", lst3)
+
+#3
+print('Enter 10 numbers between 1 and 12')
+number_lisr=[]
+for i in range(10):
+    number+int(input('Enter a number'))
+    number_list.append(number)
+print(number_list)
